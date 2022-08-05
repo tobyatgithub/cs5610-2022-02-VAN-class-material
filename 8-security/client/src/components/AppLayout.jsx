f5a34bacb8700d479eb33cc74160b22cc179e2b4
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2022-08-04 18:31:41 -0700

    Web  Security

---
+++ 8-security/client/src/components/AppLayout.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import "../style/appLayout.css";
+
+export default function AppLayout() {
+  const { user, isLoading, logout } = useAuth0();
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="app">
+      <div className="title">
+        <h1>NEU Github App</h1>
+      </div>
+      <div className="header">
+        <nav className="menu">
+          <ul className="menu-list">
+            <li>
+              <Link to="/app">Profile</Link>
+            </li>
+            <li>
+              <Link to="/app/todos">TODOs</Link>
+            </li>
+            <li>
+              <Link to="/app/debugger">Auth Debugger</Link>
+            </li>
+            <li>
+              <button
+                className="exit-button"
+                onClick={() => logout({ returnTo: window.location.origin })}
+              >
+                LogOut
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <div>Welcome 👋 {user.name} </div>
+      </div>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
